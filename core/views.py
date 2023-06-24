7f6e8979519f553f2bd31c750fe3a64b4dee985f
sphinx-austin <austinjuniour4@gmail.com>
2023-06-24 19:25:11 +0300

    update test

--- core/views.py
+++ core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def test_view(request):
+    data = {
+        'name' : 'james',
+        'city' : 'nairobi',
+        'address' : 123
+    }
+
+    return JsonResponse(data)
