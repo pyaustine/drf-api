438ab1b5815b184615b88ccae0b4f1f28d5d3eb5
sphinx-austin <austinjuniour4@gmail.com>
2023-06-25 17:31:35 +0300

    update

--- core/views.py
+++ core/views.py
@@ -6,6 +6,9 @@ from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from . serializers import PostSerializer
+from . models import Post
+
 # Create your views here.
 class TestView(APIView):
     def get(self, request, *args, **kwargs):
@@ -15,6 +18,13 @@ class TestView(APIView):
         'address' : 123
         }
         return Response(data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
 
 
   
