4c46142c33f144a6603990e9960dd76652d9a9fd
sphinx-austin <austinjuniour4@gmail.com>
2023-06-25 16:38:10 +0300

    testview

--- core/views.py
+++ core/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-# Create your views here.
 
-def test_view(request):
-    data = {
+# third party imports
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+# Create your views here.
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
         'name' : 'james',
         'city' : 'nairobi',
         'address' : 123
-    }
+        }
+        return Response(data)
 
-    return JsonResponse(data)
+
+  
+# def test_view(request):
+#     data = {
+#         'name' : 'james',
+#         'city' : 'nairobi',
+#         'address' : 123
+#     }
+
+#     return JsonResponse(data)
