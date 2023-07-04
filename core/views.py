4da8fde61bd1f44ac84c8e46f764772fb8cb6876
sphinx-austin <austinjuniour4@gmail.com>
2023-07-04 21:17:30 +0300

    update

--- core/views.py
+++ core/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 
 
 # third party imports
+from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,6 +12,9 @@ from . models import Post
 
 # Create your views here.
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         post = qs.first()
