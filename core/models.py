b85a99a1812d69bca6a14b6477787a39f9a83d6c
sphinx-austin <austinjuniour4@gmail.com>
2023-07-04 21:17:09 +0300

    update

--- core/models.py
+++ core/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     title = models.CharField(max_length=256)
     description = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
